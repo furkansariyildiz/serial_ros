2ac92f5238451846a6499ee2b22d5e69b0e882f4
furkansariyildiz <furkansariyildiz@windowslive.com>
2023-07-26 21:42:43 +0300

    Initialize package.

---
+++ include/serial_ros2/serial_ros2.h
@@ -0,0 +1,35 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+#include <rclcpp/rclcpp.hpp>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fcntl.h>
+#include <termios.h>
+
+
+using namespace std;
+
+class Serial
+{
+    private:
+        int serial_port_;
+        int baudrate_;
+
+
+    protected:
+
+    public:
+        Serial(string port_name, int baudrate);
+        ~Serial(void);
+
+        int openSerialPort(void);
+        int prepareSerialPort(void);
+        int writeSerialPort(string data);
+        int readSerialPort(void);
+
+
+};
+
+#endif
---
+++ src/serial_ros2.cpp
@@ -0,0 +1,6 @@
+#include <serial_ros2/serial_ros2.h>
+
+Serial::Serial(string port_name, int baudrate)
+{
+    
+}
